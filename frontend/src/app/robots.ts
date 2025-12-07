bbbe652ae4007d7d055d761aeb2d967915039d51
ABDULLAH ABDULFATAH <Boomjay008@gmail.com>
2025-12-07 21:59:07 +0100

    Update robots.ts

--- frontend/src/app/robots.ts
+++ frontend/src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/admin/', '/dashboard/', '/hifdh/'], // Don't index private/admin pages
     },
-    sitemap: 'https://atqin.vercel.app/sitemap.xml',
+    sitemap: 'https://iqraa.space/sitemap.xml',
   };
 }
