531dee26d4c91096a5d37d7d1016e06d81bf6ad5
Abdullah Abdulfatah <abdullahabdulfatah526@gmail.com>
2025-12-07 01:28:31 +0100

    feat: Implement actual password reset email sending and add SMTP configuration.

--- backend/internal/mailer/mailer.go
+++ backend/internal/mailer/mailer.go
@@ -6,40 +6,38 @@ import (
 )
 
 type Mailer struct {
-	dialer *smtp.Auth
-	sender string
+	host     string
+	port     int
+	username string
+	password string
+	sender   string
 }
 
 func New(host string, port int, username, password, sender string) Mailer {
-	auth := smtp.PlainAuth("", username, password, host)
 	return Mailer{
-		dialer: &auth,
-		sender: sender,
+		host:     host,
+		port:     port,
+		username: username,
+		password: password,
+		sender:   sender,
 	}
 }
 
-func (m Mailer) Send(recipient, templateFile string, data any) error {
-	// For now, we will just log the email content to stdout as requested in the plan
-	// since we might not have a real SMTP server configured in dev.
-	// In a real implementation, we would parse templates and send actual emails.
-	
-	// If we wanted to send real emails:
-	// msg := []byte("To: " + recipient + "\r\n" +
-	// 	"Subject: Reset your password\r\n" +
-	// 	"\r\n" +
-	// 	fmt.Sprintf("%v", data) + "\r\n")
-	// addr := fmt.Sprintf("%s:%d", "smtp.example.com", 587)
-	// err := smtp.SendMail(addr, *m.dialer, m.sender, []string{recipient}, msg)
-	// return err
-	
-	return nil
-}
-
-// SendPasswordReset sends a password reset email (simulated)
+// SendPasswordReset sends a password reset email
 func (m Mailer) SendPasswordReset(recipient, token string) error {
-	fmt.Printf("----------------------------------------\n")
-	fmt.Printf("Sending Password Reset Email to %s\n", recipient)
-	fmt.Printf("Token: %s\n", token)
-	fmt.Printf("----------------------------------------\n")
+	addr := fmt.Sprintf("%s:%d", m.host, m.port)
+	auth := smtp.PlainAuth("", m.username, m.password, m.host)
+
+	msg := []byte("To: " + recipient + "\r\n" +
+		"Subject: Reset your password\r\n" +
+		"From: " + m.sender + "\r\n" +
+		"\r\n" +
+		"Use the following token to reset your password: " + token + "\r\n")
+
+	err := smtp.SendMail(addr, auth, m.sender, []string{recipient}, msg)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
